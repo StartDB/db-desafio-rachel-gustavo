d21b05c0e908925b3145bfa3c3abd71da259fd44
gusilvo <gusilvo@LAPTOP-EHAKFTM5.>
2024-12-10 17:58:48 -0300

    Função para registro criada final #4

--- backend/src/main/java/com/cuidarmais/demo/Entities/User.java
+++ backend/src/main/java/com/cuidarmais/demo/Entities/User.java
@@ -10,8 +10,6 @@ import com.cuidarmais.demo.Entities.EntitiyObjects.Enums.Role;
 
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
