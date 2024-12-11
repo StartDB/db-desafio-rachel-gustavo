49556c320850f2227179baf7c14bd150f68fdabc
gusilvo <gusilvo@LAPTOP-EHAKFTM5.>
2024-12-11 11:01:12 -0300

    Criado o Controller do Volunteer

---
+++ backend/src/main/java/com/cuidarmais/demo/Controllers/VolunteerController.java
@@ -0,0 +1,36 @@
+package com.cuidarmais.demo.Controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cuidarmais.demo.Entities.Volunteer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+public class VolunteerController {
+    
+    @Autowired
+    private VolunteerService volunteerService;
+
+    @GetMapping("/listAll")
+    public List<Volunteer> getListVolunteer() {
+        return volunteerService.listAll();
+    }
+
+    @PostMapping("/save")
+    public Volunteer saveVolunteer(@RequestBody Volunteer volunteer) {
+        
+        volunteerService.save();
+
+        return volunteer;
+    }
+    
+    
+}
