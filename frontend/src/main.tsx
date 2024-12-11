f90873ec56dbf795c6574cbc5be5ede3db81abf2
Rachel Pizane <rachel.pzm@gmail.com>
2024-12-10 22:47:18 -0300

    Adicionei a rota do login e dashboard

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Register from './pages/Register.tsx'
 import { Login } from './pages/Login.tsx'
+import { Dashboard} from './pages/Dashboard.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,6 +14,8 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/" element={<App />}>
             <Route path="cadastro" element = {<Register/>} />
             <Route path="login" element = {<Login/>} />
+            <Route path="dashboard/:id" element={<Dashboard />}>
+            </Route>
           </Route>
       </Routes>
     </BrowserRouter>
