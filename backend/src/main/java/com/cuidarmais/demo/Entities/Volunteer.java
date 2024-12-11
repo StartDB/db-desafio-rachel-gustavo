63699c560ea1157712ee1b0538ee288e0092ef62
gusilvo <gusilvo@LAPTOP-EHAKFTM5.>
2024-12-11 11:31:10 -0300

    Finalizado base do Voluntario

--- backend/src/main/java/com/cuidarmais/demo/Entities/Volunteer.java
+++ backend/src/main/java/com/cuidarmais/demo/Entities/Volunteer.java
@@ -1,10 +1,8 @@
 package com.cuidarmais.demo.Entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "Volunteer")
 public class Volunteer extends User {
 
     private double rating;
