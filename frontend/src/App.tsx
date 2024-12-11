d5624fba62150bc1048a932937b3e384867c040a
Rachel Pizane <rachel.pzm@gmail.com>
2024-12-11 20:45:54 -0300

    criei o contexto necessário para disponibilizar o objeto User para a aplicação de forma global

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router'
 import './App.css'
 import { NavBar } from './components/NavBar'
-// import { useState } from 'react';
+//import { useState } from 'react';
 
 function App() {
   return (
