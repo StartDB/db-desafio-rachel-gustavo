f261924124a89cd3218b08f6ad0468ad46f1e150
Rachel Pizane <rachel.pzm@gmail.com>
2024-12-10 22:25:02 -0300

    pagina do login inicializada com as tag bases

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
 export function Login(){
+
+    const [id, setId] = useState('');
+    const navigate = useNavigate();
+
+    function handleLogin() {
+        if (id) {
+          console.log()
+          navigate(`/dashboard/${id}`);
+        } else {
+          alert('Por favor, insira um ID');
+        }
+    };
+
     return (
         <form>
             <h1>Login</h1>
-            <input type="text" />
-            <input type="password" />
-            <input type="button" value="Entrar"/>
+
+            <div>
+                <label>Nome do Usuário</label>
+                <input type="text" />
+            </div>
+
+            <div>
+                <label>Senha</label>
+                <input type="password" />
+            </div>
+
+            <button>Entrar</button>
         </form>
     )
 }
