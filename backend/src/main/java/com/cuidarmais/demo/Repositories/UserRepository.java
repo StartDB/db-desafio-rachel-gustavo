d6c366754454b4662c74998483fdb2c99271d1d4
gusilvo <gusilvo@LAPTOP-EHAKFTM5.>
2024-12-11 17:57:34 -0300

    Query tentando pegar a Role para o Login #13

--- backend/src/main/java/com/cuidarmais/demo/Repositories/UserRepository.java
+++ backend/src/main/java/com/cuidarmais/demo/Repositories/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.cuidarmais.demo.Entities.User;
+import com.cuidarmais.demo.Entities.EntityObjects.Enums.Role;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -12,4 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select u from User u where u.username = :username and u.password = :password")
     public User findLogin(String username, String password);
 
+    @Query("select u.role from User u where u.username =: username")
+    public Role findRoleByUsername(String username);
 }
