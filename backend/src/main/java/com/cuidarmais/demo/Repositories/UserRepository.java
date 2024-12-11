303cd18a141c578b815306571d386fa60c8f5a24
gusilvo <gusilvo@LAPTOP-EHAKFTM5.>
2024-12-11 17:32:19 -0300

    Adicionado o metodo no repositorio de encontrar o Login

--- backend/src/main/java/com/cuidarmais/demo/Repositories/UserRepository.java
+++ backend/src/main/java/com/cuidarmais/demo/Repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.cuidarmais.demo.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.cuidarmais.demo.Entities.User;
@@ -8,4 +9,6 @@ import com.cuidarmais.demo.Entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("select u from users u where u.username = :username and u.password = :password")
+    public User findLogin(String username, String senha);
 }
