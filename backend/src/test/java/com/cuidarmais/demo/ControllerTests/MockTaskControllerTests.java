5f63eb80259e95964fd4cde9200479a634c81408
gusilvo <gusilvo@LAPTOP-EHAKFTM5.>
2024-12-12 19:03:15 -0300

    Teste sem integração do TaskController #27

---
+++ backend/src/test/java/com/cuidarmais/demo/ControllerTests/MockTaskControllerTests.java
@@ -0,0 +1,38 @@
+package com.cuidarmais.demo.ControllerTests;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.cuidarmais.demo.Controllers.TaskController;
+import com.cuidarmais.demo.Entities.Task;
+import com.cuidarmais.demo.Services.TaskService;
+
+@WebMvcTest(TaskController.class)
+public class MockTaskControllerTests {
+    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private TaskService taskService;
+
+    @Test
+    void getTasks() throws Exception {
+        List<Task> mockList = new ArrayList<Task>();
+        when(taskService.getTasks(null, null)).thenReturn(mockList);
+        this.mockMvc.perform(get("/task/SupportTypeOrStatusFilter"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$").isArray());
+        }
+    }
