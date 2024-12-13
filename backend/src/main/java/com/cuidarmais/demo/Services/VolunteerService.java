3f088cdefb6a3ca28e3f415b00e297484871e523
gusilvo <gusilvo@LAPTOP-EHAKFTM5.>
2024-12-13 16:09:39 -0300

    Att

--- backend/src/main/java/com/cuidarmais/demo/Services/VolunteerService.java
+++ backend/src/main/java/com/cuidarmais/demo/Services/VolunteerService.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.cuidarmais.demo.Entities.Elderly;
 import com.cuidarmais.demo.Entities.Volunteer;
 import com.cuidarmais.demo.Repositories.VolunteerRepository;
 
