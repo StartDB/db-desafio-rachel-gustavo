4dd6f9a4f05fa1ee0658a84a01296092a7d15f10
Rachel Pizane <rachel.pzm@gmail.com>
2024-12-12 22:18:44 -0300

    estilizei as barras de navageção e iniciei os estilos globais

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,14 +1,23 @@
 import { NavLink } from "react-router";
 import useUser from "../contexts/hook/useUser";
+import styles from './NavBar.module.css';
 
 export function NavBar() {
     const { user } = useUser();
 
     return (
-        <nav>
-            <NavLink to="/">Página Inicial</NavLink>
-            <NavLink to="/cadastro">Cadastrar</NavLink>
-            <NavLink to={user ? "/" : "/login" }>{user ? "Sair" : "Login" }</NavLink>
+        <nav className={styles.container}>
+            <NavLink to="/" className={`${styles.link} link-nav`}>Página Inicial</NavLink>
+
+            <div className={styles.col}>
+                <NavLink to="/" className={`${styles.link} link-nav`}>Sobre nós</NavLink>
+                <NavLink to="/" className={`${styles.link} link-nav`}>Como funciona?</NavLink>
+            </div>
+
+            <div className={styles.col}>
+                <NavLink to="/cadastro" className={`${styles.link} link-nav`}>Cadastrar</NavLink>
+                <NavLink to={user ? "/" : "/login" } className={`${styles.link} link-nav`}>{user ? "Sair" : "Login" }</NavLink>
+            </div>
         </nav>
     )
 }
