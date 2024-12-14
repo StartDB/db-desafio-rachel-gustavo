43e97b3d961bbe71f90ab23b8bb486bf04c80ea2
gusilvo <gusilvo@LAPTOP-EHAKFTM5.>
2024-12-14 12:00:42 -0300

    mini correção

--- backend/src/main/java/com/cuidarmais/demo/DTO/UpdateUserDTO.java
+++ backend/src/main/java/com/cuidarmais/demo/DTO/UpdateUserDTO.java
@@ -4,34 +4,116 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.cuidarmais.demo.Entities.Elderly;
 import com.cuidarmais.demo.Entities.Task;
+import com.cuidarmais.demo.Entities.Volunteer;
 import com.cuidarmais.demo.Entities.EntityObjects.Address;
 
+public record UpdateUserDTO(
 
-public record UpdateUserDTO (Long id,
+        Long id,
 
-String firstName,
+        String firstName,
 
-String lastName,
+        String lastName,
 
-String username,
+        String username,
 
-String password,
+        String password,
 
-String email,
+        String email,
 
-int phone,
+        int phone,
 
-LocalDate birthdate,
+        LocalDate birthdate,
 
-Address address,
+        Address address,
 
-String description,
+        String description,
 
-LocalDateTime createdAt,
+        LocalDateTime createdAt,
 
-List<Task> tasks) {
+        List<Task> tasks,
+
+        double rating) {
+
+    public static Volunteer mergeUserToEntity(UpdateUserDTO userDTO, Volunteer volunteer) {
+        if (userDTO.firstName() != null) {
+            volunteer.setFirstName(userDTO.firstName());
+        }
+        if (userDTO.lastName() != null) {
+            volunteer.setLastName(userDTO.lastName());
+        }
+        if (userDTO.username() != null) {
+            volunteer.setUsername(userDTO.username());
+        }
+        if (userDTO.password() != null) {
+            volunteer.setPassword(userDTO.password());
+        }
+        if (userDTO.email() != null) {
+            volunteer.setEmail(userDTO.email());
+        }
+        if (userDTO.phone() != 0) { // Assumindo que 0 não é um número de telefone válido
+            volunteer.setPhone(userDTO.phone());
+        }
+        if (userDTO.birthdate() != null) {
+            volunteer.setBirthdate(userDTO.birthdate());
+        }
+        if (userDTO.address() != null) {
+            volunteer.setAddress(userDTO.address());
+        }
+        if (userDTO.description() != null) {
+            volunteer.setDescription(userDTO.description());
+        }
+        if (userDTO.createdAt() != null) {
+            volunteer.setCreatedAt(userDTO.createdAt());
+        }
+        if (userDTO.tasks() != null && !userDTO.tasks().isEmpty()) {
+            volunteer.setTasks(userDTO.tasks());
+        }
+        if (userDTO.rating() != 0) {
+            volunteer.setRating(userDTO.rating());
+        }
     
-    
+        return volunteer;
+    }
 
+    public static Elderly mergeUserToEntity(UpdateUserDTO userDTO, Elderly elderly) {
+        if (userDTO.firstName() != null) {
+            elderly.setFirstName(userDTO.firstName());
+        }
+        if (userDTO.lastName() != null) {
+            elderly.setLastName(userDTO.lastName());
+        }
+        if (userDTO.username() != null) {
+            elderly.setUsername(userDTO.username());
+        }
+        if (userDTO.password() != null) {
+            elderly.setPassword(userDTO.password());
+        }
+        if (userDTO.email() != null) {
+            elderly.setEmail(userDTO.email());
+        }
+        if (userDTO.phone() != 0) {
+            elderly.setPhone(userDTO.phone());
+        }
+        if (userDTO.birthdate() != null) {
+            elderly.setBirthdate(userDTO.birthdate());
+        }
+        if (userDTO.address() != null) {
+            elderly.setAddress(userDTO.address());
+        }
+        if (userDTO.description() != null) {
+            elderly.setDescription(userDTO.description());
+        }
+        if (userDTO.createdAt() != null) {
+            elderly.setCreatedAt(userDTO.createdAt());
+        }
+        if (userDTO.tasks() != null && !userDTO.tasks().isEmpty()) {
+            elderly.setTasks(userDTO.tasks());
+        }
+    
+        return elderly;
+    }
+            
 }
