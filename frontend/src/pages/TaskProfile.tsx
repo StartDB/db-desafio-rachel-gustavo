c012133cc5a375cbd862f869f7dc1fc0ca1208f0
Rachel Pizane <rachel.pzm@gmail.com>
2024-12-16 10:11:57 -0300

    inclui navegação para a pagina anterior

--- frontend/src/pages/TaskProfile.tsx
+++ frontend/src/pages/TaskProfile.tsx
@@ -1,12 +1,20 @@
+import { useNavigate } from "react-router";
 import MainTitle from "../components/MainTitle";
 import styles from './TaskProfile.module.css';
 
 export default function TaskProfile() {
-    
+    const navigate = useNavigate();
+
+    function returnPreviousPage(e: React.MouseEvent<HTMLAnchorElement>): void{
+        e.preventDefault()
+        navigate(-1)
+    }
+
     return (
         <main className={styles.containerBackgroundMain}>
             <section className="container-section-base">
                 <MainTitle content="Página da Tarefa" />
+                <a href="#" onClick={returnPreviousPage}>Voltar</a>
             </section>
         </main>
     )
